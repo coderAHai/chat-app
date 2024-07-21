ca6fb25bbc0835d8cfe31b872d6f07f0d52d7e58
coderAHai <coderyue@qq.com>
2024-07-21 16:12:30 +0800

    登录&注册功能对接AAPI

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -4,18 +4,65 @@ import logo from '../../assets/images/logo.png'
 import { useState } from 'react'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import { toast } from 'sonner'
+import server from '@/utils/server'
+import { LOGIN_ROUTE, SIGNUP_ROUTE } from '@/utils/constants'
+import { useNavigate } from 'react-router-dom'
 
 const Auth = () => {
+  const navigate = useNavigate()
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPssword] = useState('')
 
+  const validateLogin = () => { 
+    if (!email.length) {
+      toast.error("Email is required.");
+      return false;
+    }
+    if (!password.length) { 
+      toast.error("Password is required.");
+      return false;
+    }
+    return true;
+  }
+
+  const validateSignup = () => { 
+    if (!email.length) {
+      toast.error("Email is required.");
+      return false;
+    }
+    if (!password.length) { 
+      toast.error("Password is required.");
+      return false;
+    }
+    if (password !== confirmPassword) {
+      toast.error("Password and confirm password should be same.");
+      return false;
+    }
+    return true;
+  }
+
   const handleLoginClick = async () => { 
-    console.log("Login");
+    if (validateLogin()) {
+      const response = await server.post(LOGIN_ROUTE, { email, password }, { withCredentials: true });
+      if (response.data.user.profileSetup) {
+        navigate("/chat");
+      } else {
+        navigate("/profile");
+      }
+      console.log(response);
+    }
   }
 
   const handleSignupClick = async () => { 
-    console.log("Signup");
+    if (validateSignup()) {
+      const response = await server.post(SIGNUP_ROUTE, { email, password }, { withCredentials: true });
+      if (response.status === 201) { 
+        navigate("/profile");
+      }
+      console.log(response);
+    }
   }
 
   return (
@@ -32,7 +79,7 @@ const Auth = () => {
             </div>
           </div>
           <div className='flex justify-center items-center w-full'>
-            <Tabs className='w-3/4'>
+            <Tabs className='w-3/4' defaultValue='login'>
               <TabsList className="w-full bg-transparent rounded-none mb-4">
                 <TabsTrigger value="login" className="w-full text-black text-opacity-90 border-b-2 rounded-none p-3 transition-all duration-300 data-[state=active]:bg-transparent data-[state=active]:font-semibold data-[state=active]:border-b-purple-500">Login</TabsTrigger>
                 <TabsTrigger value="signup" className="w-full text-black text-opacity-90 border-b-2 rounded-none p-3 transition-all duration-300 data-[state=active]:bg-transparent data-[state=active]:font-semibold data-[state=active]:border-b-purple-500">Signup</TabsTrigger>
---
+++ src/utils/server.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+import { HOST } from './constants';
+
+const server = axios.create({
+  baseURL: HOST
+});
+
+export default server;
